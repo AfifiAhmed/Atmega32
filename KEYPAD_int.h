457cfe59c8b797f0bb86ed37eda3937bd0f33bf9
AfifiAhmed <111084350+AfifiAhmed@users.noreply.github.com>
2022-10-21 18:07:14 +0200

    Add files via upload

---
+++ KEYPAD_int.h
@@ -0,0 +1,16 @@
+/****************************************/
+/*          Author: Ahmed Afifi         */        
+/*          Date: 8-4-2022              */
+/*          Version: 1.0                */
+/*          Module: KEYPAD              */
+/****************************************/
+
+
+#ifndef KEYPAD_INT_H_
+#define KEYPAD_INT_H_
+
+#define KEYPAD_U8_NULL '\0'
+void KEYPAD_voidInit(void);
+u8 KEYPAD_u8GetPressedButton(void);
+
+#endif
---
+++ KEYPAD_private.h
@@ -0,0 +1,15 @@
+/****************************************/
+/*          Author: Ahmed Afifi         */        
+/*          Date: 8-4-2022              */
+/*          Version: 1.0                */
+/*          Module: KEYPAD              */
+/****************************************/
+
+
+#ifndef KEYPAD_PRIVATE_H_
+#define KEYPAD_PRIVATE_H_
+
+#define KEYPAD_U8_INTERNAL_RES  0
+#define KEYPAD_U8_EXTERNAL_RES  1
+
+#endif

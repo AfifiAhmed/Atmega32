e93a9f740d9512314b6e614bcdc151d571b62861
AfifiAhmed <111084350+AfifiAhmed@users.noreply.github.com>
2022-10-24 18:11:51 +0200

    Add files via upload

---
+++ SPI_int.h
@@ -0,0 +1,11 @@
+/****************************************/
+/*          Author: Ahmed Afifi         */        
+/*          Date: 5-5-2022              */
+/*          Version: 1.0                */
+/*          Module: SPI                 */
+/****************************************/
+
+void SPI_voidInit(void);
+void SPI_voidEnable(void);
+void SPI_voidDisable(void);
+u8 SPI_u8DataTransfere(u8 Local_u8Data);

f2dbc4d4abc45de67391a95f07e05fd38173004c
AfifiAhmed <111084350+AfifiAhmed@users.noreply.github.com>
2022-10-24 18:14:12 +0200

    Add files via upload

---
+++ LCD_private.h
@@ -0,0 +1,13 @@
+/****************************************/
+/*          Author: Ahmed Afifi         */        
+/*          Date: 28-4-2022             */
+/*          Version: 1.0                */
+/*          Module: LCD                 */
+/****************************************/
+#ifndef LCD_PRIVATE_H_
+#define LCD_PRIVATE_H_
+
+ #define LCD_U8_4BIT 0
+ #define LCD_U8_8BIT 1
+
+#endif

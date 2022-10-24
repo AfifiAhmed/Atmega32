84015dbaecb73ed7494ffec350a9bc53822d32f8
AfifiAhmed <111084350+AfifiAhmed@users.noreply.github.com>
2022-10-24 18:03:21 +0200

    Add files via upload

---
+++ SEG_int.h
@@ -0,0 +1,13 @@
+/****************************************/
+/*          Author: Ahmed Afifi         */        
+/*          Date: 15-4-2022             */
+/*          Version: 1.0                */
+/*          Module: SEGMENT 7           */
+/****************************************/
+#ifndef SEG_INT_H_
+#define SEG_INT_H_
+
+void SEG_voidEnSeg_1(void);
+void SEG_voidEnSeg_2(void);
+
+#endif

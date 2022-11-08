0f6f3d91b3b404f55f8385a1d9fa99878cd96c95
AfifiAhmed <111084350+AfifiAhmed@users.noreply.github.com>
2022-11-08 10:57:28 +0100

    Delete GIE_config.h

--- GIE_config.h
+++
@@ -1,8 +0,0 @@
-/****************************************/
-/*          Author: Ahmed Afifi         */        
-/*          Date: 28-05-2022            */
-/*          Version: 1.0                */
-/*          Module: Interrupt           */
-/****************************************/
-
-//GIE - General Interrupt Enable
